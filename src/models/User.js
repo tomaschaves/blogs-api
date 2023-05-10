48ac73e676ee1d05af6f972ab79dc5fe84fedddb
Tomás Chaves <tomcchaves@gmail.com>
2023-05-10 17:52:17 -0300

    Requisito 2.

---
+++ src/models/User.js
@@ -0,0 +1,19 @@
+/**
+ * @param {import('sequelize').QueryInterface} queryInterface
+ * @param {import('sequelize').Sequelize} Sequelize
+ */
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User', {
+      id: DataTypes.INTEGER,
+      display_name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+  }, {
+      timestamps: false,
+      tableName: 'users',
+    });
+  return User;
+};
